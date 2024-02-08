7d70d46777d642f47b18af261b047622e0f687b6
Roman Tereshkov <tereshkov291993@gmail.com>
2024-02-08 21:06:01 +0300

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,32 +4,19 @@ import styled from 'styled-components';
 const options = {
 	body: 'click',
 	icon: 'https://mc21.ru/lk/static/media/logo.b902c3da117162aa84f03ae2f8a54f1c.svg',
-	onсlick: () => console.log('Notification clicked'),
-};
-const defaultOptions = {
-	body: 'click',
-	icon: 'https://mc21.ru/lk/static/media/logo.b902c3da117162aa84f03ae2f8a54f1c.svg',
-	onсlick: () => console.log('Notification clicked'),
+	// onсlick: (e) => console.log('Notification clicked'),
 };
 
-const notifyUser = async (notificationText, options) => {
+const notifyUser = async (notificationText = 'Медицинский центр «XXI век»', options) => {
 	if (!('Notification' in window)) {
 		alert('This browser does not support desktop notification');
-	} else if (Notification.permission === 'granted' || Notification.permission === 'default') {
+	} else if (Notification.permission === 'granted') {
+		const notification = new Notification(notificationText, options);
+	} else if (Notification.permission !== 'denied') {
 		await Notification.requestPermission().then((permission) => {
-			console.log({ bottom: permission });
 			if (permission === 'granted') {
-				const notification = new Notification(notificationText, options);
-				notification.addEventListener('click', options?.onсlick);
-			}
-		});
-	} else if (Notification.permission === 'denied') {
-		await Notification.requestPermission().then((permission) => {
-			console.log({ bottom: permission });
-			if (permission === 'denied') {
-				const notification = new Notification(notificationText, options);
-				notification.addEventListener('error', (e) => console.log(e));
-				notification.addEventListener('click', options?.onсlick);
+				const notification = new Notification(notificationText, { ...options, body: '123' });
+				// notification.addEventListener('click', options?.onсlick);
 			}
 		});
 	}
@@ -39,39 +26,27 @@ function App() {
 	const [userResponded, setUserResponded] = useState(false);
 
 	const enableNotificationsAndClose = async () => {
-		await notifyUser('Медицинский центр «XXI век»', {
-			body: 'Вы подписались на уведомления',
-			icon: 'https://mc21.ru/lk/static/media/logo.b902c3da117162aa84f03ae2f8a54f1c.svg',
-		}).then(() => {
+		await notifyUser().then(() => {
 			setUserResponded(true);
 		});
 	};
 
 	const disableNotificationsAndClose = async () => {
-		await notifyUser('Медицинский центр «XXI век»', {
-			body: 'Вы отписались от уведомлений',
-			icon: 'https://mc21.ru/lk/static/media/logo.b902c3da117162aa84f03ae2f8a54f1c.svg',
-		}).then(() => {
-			setUserResponded(true);
-		});
+		setUserResponded(true);
 	};
 
 	return (
 		<div>
-			{Notification.permission === 'granted' ? (
+			{!userResponded && Notification.permission !== 'granted' ? (
+				<StyledNotificationPermission>
+					Notifications:
+					<button onClick={enableNotificationsAndClose}>YES</button>
+					<button onClick={disableNotificationsAndClose}>NO</button>
+				</StyledNotificationPermission>
+			) : Notification.permission === 'granted' ? (
 				<>
-					<StyledNotificationPermission>
-						Notifications:
-						<button onClick={enableNotificationsAndClose}>YES</button>
-						<button onClick={disableNotificationsAndClose}>NO</button>
-					</StyledNotificationPermission>
-
-					<>
-						<div>You have enabled notifications</div>
-						<button onClick={() => notifyUser('First notification message', options)}>
-							Click to show notification
-						</button>
-					</>
+					<div>You have enabled notifications</div>
+					<button onClick={() => notifyUser(undefined, options)}>Click to show notification</button>
 				</>
 			) : (
 				<div>You have disabled notifications</div>
